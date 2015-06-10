e6bb2a19da0db55d96d18484630b2b2f0f4563f9
Jonathan Price <jonathan@jonathanprice.uk>
2015-06-10 20:08:58 +0000

    Fixed font attribute inheritance in the unordered lists

--- computing/index.php
+++ computing/index.php
@@ -7,6 +7,10 @@
             <div class="col s12 m8 l9">
               <h1 class="light grey-text text-darken-4 header">Computing</h1>
               <h2 class="light grey-text text-darken-4 header">FreeBSD</h2>
+              <p class="light grey-text text-darken-4">    
+                What is FreeBSD? To quote the official <a href="http://www.freebsd.org/">site</a>, "FreeBSD is an advanced computer operating system used to power modern servers, desktops and embedded platforms."
+              </p>
+                
               <p class="light grey-text text-darken-4">
                 I first came in to contact with FreeBSD in 2012, as a volunteer for <a href="http://jupiterbroadcasting.com/">Jupiter Broadcasting</a>.
                 Since then, I have developed a detailed understanding of the operating system, and contribute
@@ -17,9 +21,10 @@
                 One way in which I contribute is with the ports tree, which is the system
                 used by FreeBSD to obtain 3rd party software (roughly analogous to the package repos of various
                 Linux distros). If I have any issues with any software I use, I will either submit an issue, or
-                if I have the skills to resolve it, a patch. Two ports that I have contributed to are www/owncloud and net/openldap24-server.
+                if I have the skills to resolve it, a patch. Two ports that I have contributed to are <a href="http://svnweb.freebsd.org/ports/head/www/owncloud/">www/owncloud</a>
+                and <a href="http://svnweb.freebsd.org/ports/head/net/openldap24-server">net/openldap24-server</a>.
                 In addition to this, I am the maintainer for the following ports:
-                <ul>
+                <ul class="light grey-text text-darken-4">
                   <li><i class="mdi-navigation-chevron-right"></i><a href="http://svnweb.freebsd.org/ports/head/security/d0_blind_id/">security/do_blind_id</a></li>
                   <li><i class="mdi-navigation-chevron-right"></i><a href="http://svnweb.freebsd.org/ports/head/games/minecraft-server/">games/minecraft-server</a></li>
                 </ul>
@@ -29,11 +34,12 @@
                 This website is actually powered by FreeBSD, running a combination of nginx and php-fpm. I try
                 to self-host where I can (in an administration sense, not a geographical one), for both
                 educational and privacy reasons. This means that I have configured a variety of services, including:
-                <ul>
+                <ul class="light grey-text text-darken-4">
                   <li><i class="mdi-navigation-chevron-right"></i>Web server (nginx and php-fpm)</li>
                   <li><i class="mdi-navigation-chevron-right"></i>Mail server (postfix, dovecot, spamassassin, opendkim)</li>
                   <li><i class="mdi-navigation-chevron-right"></i>Monitoring software (nagios)</li>
-                  <li><i class="mdi-navigation-chevron-right"></i>File storage (OwnCloud)<li>
+                  <li><i class="mdi-navigation-chevron-right"></i>File storage (OwnCloud)</li>
+                  <li><i class="mdi-navigation-chevron-right"></i>Issue tracker (BugZilla)</li>
                   <li><i class="mdi-navigation-chevron-right"></i>Central LDAP authentication (OpenLDAP, used for <a href="https://jonathanprice.org">JonathanPrice.org</a>, including OwnCloud, BugZilla, etc.)</li>
                 </ul>
               </p>
@@ -63,7 +69,7 @@
                 In particular, I consider myself rather adept at shell scripting. For portability reasons I try to keep conformant
                 with the bourne shell, although in certain cases I will instead develop for bash. One particular project I felt
                 would be useful for other administrators, so have published it under an open source license on GitHub:
-                <ul>
+                <ul class="light grey-text text-darken-4">
                   <li><i class="mdi-navigation-chevron-right"></i><a href="https://github.com/pricetx/backup">Backup | Simple backup script utilising
                     OpenSSL, tar and rsync, written in bash</a></li>
                 </ul>
