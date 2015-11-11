9041a2154ba14d3daf883b7b38d1d14f5cfb06b0
Jonathan Price <jonathan@jonathanprice.uk>
2015-11-11 22:47:56 +0000

    Fixed issue with footer not staying at bottom of screen on homepage

--- index.php
+++ index.php
@@ -9,45 +9,45 @@
               <h5 class="header col s12 light">Computer Scientist, Pilot, Musician</h5>
             </div>
             <div class="row">
-
+            </div>
           </div>
-        </div>
 
-        <div class="container">
-          <div class="section">
+          <div class="container">
+            <div class="section">
 
-            <!--   Icon Section   -->
-            <div class="row">
+              <!--   Icon Section   -->
+              <div class="row">
 
-              <div class="col s12 m4">
-                <div class="icon-block">
-                  <h2 class="center"><a href="/computing" class="blue-grey-text text-darken-4"><i class="medium material-icons">desktop_windows</i></h2>
-                  <h5 class="center">Computing</h5>
+                <div class="col s12 m4">
+                  <div class="icon-block">
+                    <h2 class="center"><a href="/computing" class="blue-grey-text text-darken-4"><i class="medium material-icons">desktop_windows</i></h2>
+                    <h5 class="center">Computing</h5>
 
-                  <p class="light center-align">I have built a career in computing. I have a broad knowledge of the field, with a specialisation in computer networking.
+                    <p class="light center-align">I have built a career in computing. I have a broad knowledge of the field, with a specialisation in computer networking.
                         In this section you can find projects I have developed and articles I have written.</p></a>
+                  </div>
                 </div>
-              </div>
 
-              <div class="col s12 m4">
-                <div class="icon-block">
-                  <h2 class="center"><a href="/aviation" class="blue-grey-text text-darken-4"><i class="medium material-icons">flight</i></h2>
-                  <h5 class="center">Aviation</h5>
+                <div class="col s12 m4">
+                  <div class="icon-block">
+                    <h2 class="center"><a href="/aviation" class="blue-grey-text text-darken-4"><i class="medium material-icons">flight</i></h2>
+                    <h5 class="center">Aviation</h5>
 
-                  <p class="light center-align">I have had a passion for aviation since I was a child. I am now embarking on the journey to earning a Private Pilot License.</p></a>
+                    <p class="light center-align">I have had a passion for aviation since I was a child. I am now embarking on the journey to earning a Private Pilot License.</p></a>
+                  </div>
                 </div>
-              </div>
 
-              <div class="col s12 m4">
-                <div class="icon-block">
-                  <h2 class="center"><a href="/music" class="blue-grey-text text-darken-4"><i class="medium material-icons">music_note</i></h2>
-                  <h5 class="center">Music</h5>
+                <div class="col s12 m4">
+                  <div class="icon-block">
+                    <h2 class="center"><a href="/music" class="blue-grey-text text-darken-4"><i class="medium material-icons">music_note</i></h2>
+                    <h5 class="center">Music</h5>
 
-                  <p class="light center-align">A pianist of many styles. I have a deep interest in music and have learnt various styles including Jazz, Classical and Rock. From 
-			time to time I record audio/video and post it here. I'm also available for hire.</p></a>
+                    <p class="light center-align">A pianist of many styles. I have a deep interest in music and have learnt various styles including Jazz, Classical and Rock. From 
+			                  time to time I record audio/video and post it here. I'm also available for hire.</p></a>
+                  </div>
                 </div>
-              </div>
 
+              </div>
             </div>
           </div>
         </div>
