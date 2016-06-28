18a3ada7fa92729d25e37b0c72c4aea823cad954
Jonathan Price <jonathan@jonathanprice.uk>
2016-06-28 09:55:54 +0000

    Filled out Aviation section (finally)

--- aviation/index.php
+++ aviation/index.php
@@ -6,6 +6,27 @@
 
             <div class="col s12 m8 l9">
               <h1 class="light grey-text text-darken-4 header">Aviation</h1>
+              <p class="light grey-text text-darken-4">
+                My passion for aviation can be attributed largely to my father. I spent a large part of
+                my childhood at air shows, air museums and planespotting. Eventually, at the age of 16,
+                I had a chance to fly for myself. Through the RAF's Air Cadet program I was able to fly
+                a Vigilant T1 powered glider. From that moment I was hooked. Sadly, due to other commitments
+                I could not continue to train with the RAF, but decided to begin saving for private lessons.
+              </p>
+
+              <img alt="Grob 109B" src="/images/grob_109b.jpg" height="" width="90%" style="margin: auto;display: block;">
+              <figcaption style="text-align: center;">Example of a Grob 109B which I flew from RAF Henlow.</figcaption>
+
+              <p class="light grey-text text-darken-4">
+                In 2015 I finally took the plunge and began taking lessons at Elstree aerodrome in Hertfordshire.
+                I am currently working towards an EASA PART-FCL, which is the Europe-wide Private Pilot's License. 
+                I currently take lessons fairly infrequently as my immediate focus is on studying for and passing 
+                the nine theory exams required to take my checkride. I hope to be able to attain my license by late 2017.
+              </p>
+
+              <img alt="Piper Warrior" src="/images/gcczv.jpg" height="" width="90%" style="margin: auto;display: block;">
+              <figcaption style="text-align: center;">One of the Piper Warriors I currently train on at Elstree.</figcaption>
+              <br/>
             </div>
 
           </div>
